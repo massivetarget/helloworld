19ff4b07193de2d1bf66771df5bedc8fd1ce43cb
massivetarget <massivetarget@gmail.com>
2019-09-30 20:47:44 +0530

    add sys.path

--- Test.py
+++ Test.py
@@ -1,3 +1,5 @@
+import sys
+
 a: int = 12
 b: int = 3
 c = a + b
@@ -6,3 +8,5 @@ print('this is c ', c)
 print(type(a))
 print("this is add on 28 jul 2019")
 print("This is added on 30 sep 2019")
+
+print(sys.path)
